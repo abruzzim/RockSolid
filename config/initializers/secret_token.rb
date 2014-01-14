6ffdd2453553802244032acf41eca0453b4f0345
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-01-14 12:56:38 -0500

    Base Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocksolid5::Application.config.secret_token = '9861ab3e2746eacd85fb700c0bdc690223157d7e223541ad005b1677ab4ed1cdb012dadaed145b797f5fcbe120158eb2f7af98f7da60efda29c5ff14a41113cd'

eb3d312be317b585cbbbf0d72c0e4c61b9860ce6
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-01-16 09:27:55 -0500

    Add database seed code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@order_num   = "Q9876"
+@cust_name   = "Mario"
+@cust_street = "234 West 34"
+@cust_city   = "New York"
+@cust_state  = "NY"
+@cust_zip    = "11736"
+@sdate       = "2014-01-15"
+@ship_street = "234 West 34"
+@ship_city   = "New York"
+@ship_state  = "NY"
+@ship_zip    = "11736"
+Order.create({
+  order_num: @order_num,
+  customer_id: Customer.create({
+                  name: @cust_name,
+                  address_id: Address.create({
+                                street: @cust_street,
+                                city:   @cust_city,
+                                state:  @cust_state,
+                                zip:    @cust_zip}).id}).id,
+  delivery_id: Delivery.create({
+                  ship_date: @sdate,
+                  address_id: Address.create({
+                                street: @ship_street,
+                                city:   @ship_city,
+                                state:  @ship_state,
+                                zip:    @ship_zip
+                              }).id})
+})

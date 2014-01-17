e022040887efc94b4a03b7310c8db18d4f336a0d
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-01-17 11:01:08 -0500

    Commented Customer Flow

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,17 @@
 class CustomersController < ApplicationController
 
   def index
+    # Get collection of objects.
     @customers = Customer.all
+    # Call index template view.
   end
 
   def new
+    # Call new template view.
   end
 
   def create
+    # Create row from template view parameters.
     Address.create({
       street:      params[:street],
       city:        params[:city],
@@ -17,10 +21,12 @@ class CustomersController < ApplicationController
                     name: params[:name]
                    }).id
       })
+    # Call index template view.
     redirect_to customers_path
   end
 
   def edit
+    # Get selected object by id.
     c = Customer.find(params[:id])
     @cid    = c.id
     @name   = c.name
@@ -28,9 +34,11 @@ class CustomersController < ApplicationController
     @city   = c.addresses.first.city
     @state  = c.addresses.first.state
     @zip    = c.addresses.first.zip
+    # Call edit template view.
   end
 
   def update
+    # Update row from template view parameters.
     c = Customer.find(params[:id])
     c.name                   = params[:name]
     c.addresses.first.street = params[:street]
@@ -38,15 +46,18 @@ class CustomersController < ApplicationController
     c.addresses.first.state  = params[:state]
     c.addresses.first.zip    = params[:zip]
     c.save
+    # Call index template view.
     redirect_to customers_path
   end
 
   def destroy
+    # Delete selected object by id.
     c = Customer.find(params[:id])
     c.address_ids.each do |id|
       Address.find(id).delete
     end
     c.delete
+    # Call index template view.
     redirect_to customers_path
   end
 

afb7f8fa18a116d7244175f1b97eeb8b5df3fef2
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-01-20 11:11:43 -0500

    Re-wrote Customer Feature

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,64 +1,69 @@
+=begin
+ Filename: app/controllers/customers.rb
+ Directory: /Users/abruzzim/Documents/ga_wdi/projects/rocksolid/
+ Author: Mario Abruzzi
+ Date: 17-Jan-2014
+ Desc: Project 1
+ Notes: 
+=end
+
 class CustomersController < ApplicationController
+
   # GET /customers
   def index
-    # Get collection of objects.
     @customers = Customer.all
-    # Call index.html.erb
   end
+
+  # GET /customers/1
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   # GET /customers/new
   def new
-    # Why not Create a new object.
-    # Call new.html.erb
   end
+
   # POST /customers
   def create
-    # Create row from view parameters.
-    Address.create({
-      street:      params[:street],
-      city:        params[:city],
-      state:       params[:state],
-      zip:         params[:zip],
-      customer_id: Customer.create({
-                    name: params[:name]
-                   }).id
-      })
-    # Redirect to index.html.erb
+    customer = Customer.new
+    customer.name = params[:name]
+    customer.addresses << Address.create({
+                            street: params[:street],
+                            city:   params[:city],
+                            state:  params[:state],
+                            zip:    params[:zip]
+                            })
+    customer.save
     redirect_to customers_path
   end
+
   # GET /customers/1/edit
   def edit
-    # Get selected object by id.
-    c = Customer.find(params[:id])
-    @cid    = c.id
-    @name   = c.name
-    @street = c.addresses.first.street
-    @city   = c.addresses.first.city
-    @state  = c.addresses.first.state
-    @zip    = c.addresses.first.zip
-    # Call edit.html.erb
+    @customer = Customer.find(params[:id])
   end
+
   # PUT /customers/1
   def update
-    # Update row from template view parameters.
-    c = Customer.find(params[:id])
-    #c.name                   = params[:name]
-    c.update_attributes(name: params[:name])
-    c.addresses.first.update_attributes(street: params[:street])
-    c.addresses.first.update_attributes(city: params[:city])
-    c.addresses.first.update_attributes(state: params[:state])
-    c.addresses.first.update_attributes(zip: params[:zip])
-    # Call index.html.erb
+    customer = Customer.find(params[:id])
+    customer.update_attributes(
+      name: params[:name]
+      )
+    customer.addresses.first.update_attributes(
+      street: params[:street],
+      city:   params[:city],
+      state:  params[:state],
+      zip:    params[:zip]
+      )
     redirect_to customers_path
   end
+
   # DELETE /customers/1
   def destroy
-    # Delete selected object by id.
-    c = Customer.find(params[:id])
-    c.address_ids.each do |id|
-      Address.find(id).delete
+    customer = Customer.find(params[:id])
+    customer.address_ids.each do |id|
+      Address.find(id).destroy
     end
-    c.delete
-    # Call index.html.erb
+    customer.destroy
     redirect_to customers_path
   end
 
